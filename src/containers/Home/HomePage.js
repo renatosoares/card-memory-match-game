62c41138fbb5b553ed76045ad38e4d7608cea59f
Renato Soares <renatosoares@rits.com.br>
2021-05-26 10:16:54 -0300

    define the deck card

--- src/containers/Home/HomePage.js
+++ src/containers/Home/HomePage.js
@@ -5,7 +5,7 @@ import { Dashboard, DashboardMenu, DashboardButton } from "../../components";
 const HomePage = () => (
   <Dashboard>
     <DashboardMenu title="Card Memory Match Game">
-      <DashboardButton>start game</DashboardButton>
+      <DashboardButton to="/game">start game</DashboardButton>
     </DashboardMenu>
   </Dashboard>
 );

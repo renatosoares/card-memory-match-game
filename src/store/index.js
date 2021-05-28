b2d7e3119eb934233d9957d1c309465eae322f72
Renato Soares <renatosoares@rits.com.br>
2021-05-28 10:01:38 -0300

    define match in playing cards

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,12 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {
   cards: [
-    { id: 1, key: 1, name: "Card 1" },
-    { id: 2, key: 2, name: "Card 2" },
-    { id: 3, key: 3, name: "Card 3" },
+    { id: 1, key: 1, name: "Card 1", isActive: false, hasMatch: false },
+    { id: 2, key: 2, name: "Card 2", isActive: false, hasMatch: false },
+    { id: 3, key: 3, name: "Card 3", isActive: false, hasMatch: false },
+    { id: 1, key: 4, name: "Card 1", isActive: false, hasMatch: false },
+    { id: 2, key: 5, name: "Card 2", isActive: false, hasMatch: false },
+    { id: 3, key: 6, name: "Card 3", isActive: false, hasMatch: false },
   ],
 };
 
@@ -14,9 +17,24 @@ const gameReducer = (state = initialState, action) => {
     case "SELECT_CARD":
       const cards = state.cards.slice();
       const index = cards.findIndex((c) => c.key === action.key);
+      const otherCardIndex = cards.findIndex((c) => c.isActive && !c.hasMatch);
 
       if (index > -1) {
-        cards[index].isActive = true;
+        if (cards[index].isActive) {
+          return state;
+        }
+
+        if (otherCardIndex > -1) {
+          if (cards[index].id === cards[otherCardIndex].id) {
+            cards[index].isActive = true;
+            cards[index].hasMatch = true;
+            cards[otherCardIndex].hasMatch = true;
+          } else {
+            cards[otherCardIndex].isActive = false;
+          }
+        } else {
+          cards[index].isActive = true;
+        }
       }
 
       return {
